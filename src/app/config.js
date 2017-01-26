0242d537f491031bb6241fb229630f1723184366
nishant <nishant.singh87@gmail.com>
2017-01-26 12:49:14 +0530

    refactor app.js

---
+++ src/app/config.js
@@ -0,0 +1,7 @@
+app.value("remote", "https://findfalcone.herokuapp.com");
+app.value("requestConfig", {headers: {Accept: "application/json", "Content-Type": "application/json"}});
+
+app.run(["remote", "$http", "requestConfig","PlanetsService", "vehiclesService", function(remote, $http, requestConfig, PlanetsService, vehiclesService){
+  PlanetsService.load();
+  vehiclesService.load();
+}]);
