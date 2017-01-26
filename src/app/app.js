25041c5f189b7d9e262f9ffcb6c575da554eaec3
nishant <nishant.singh87@gmail.com>
2017-01-26 12:48:17 +0530

    browserify.

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("galaxy", []);
+window.app = angular.module("galaxy", []);
 
 // Delay before confirm action of wizard fro smoother transition.
 app.constant("wizardTimeout", 150);
@@ -27,3 +27,15 @@ app.run(["remote", "$http", "requestConfig","PlanetsService", "vehiclesService",
   PlanetsService.load();
   vehiclesService.load();
 }]);
+
+require("./result/result-service.js");
+require("./result/result-controller.js");
+require("./missions/missions-service.js");
+require("./missions/missions-controller.js");
+require("./help/help-service.js");
+require("./components/game-progress.js");
+require("./components/fullscreen.js");
+require("./planets/planets-service.js");
+require("./vehicles/vehicles-service.js");
+require("./mission-wizard/wizard-controller.js");
+require("./mission-wizard/wizard-service.js");
