713b0cfe9203be8a4cd09a8bfd7b7bdc6aa1ab6c
nishant <nishant.singh87@gmail.com>
2017-02-02 15:16:45 +0530

    Adding planet.

--- src/app/vehicles/vehicles-service.js
+++ src/app/vehicles/vehicles-service.js
@@ -1,26 +1,9 @@
-app.service("VehiclesService", ["$http", "remote", "requestConfig", "vehicleIcons", function($http, remote, requestConfig, vehicleIcons){
-	var withIcons = function(data){
-		var vehicle = {
-			name: data.name,
-			speed: data.speed,
-			icon: vehicleIcons[data.name] || vehicleIcons["other"],
-			remaining: data.total_no,
-			total_no: data.total_no,
-			max_distance: data.max_distance,
-			assign: function () {
-				--vehicle.remaining;
-			},
-			unassign: function () {
-				++vehicle.remaining;
-			}
-		};
-		return vehicle;
-	};
+app.service("VehiclesService", ["$http", "remote", "requestConfig", "Vehicle", function($http, remote, requestConfig, Vehicle){
 	var vehicles = {
 		list: [],
 		load: function(){
 			$http.get(remote + "/vehicles", {}, requestConfig).then(function(response){
-				vehicles.list = response.data.map(withIcons);
+				vehicles.list = response.data.map(Vehicle.create);
 			});
 		}
 	};
